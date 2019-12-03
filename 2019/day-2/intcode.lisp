eea6da447bbba274d7ddcd9e85f92f22cc412e1b
Mark Simpson <verdammelt@gmail.com>
2019-12-02 21:17:34 -0500

    Day 2.2 - DONE

--- 2019/day-2/intcode.lisp
+++ 2019/day-2/intcode.lisp
@@ -91,3 +91,17 @@
     (modify-memory (read-input input-file)
                    '((1 12) (2 2))))
    0))
+
+;; target from exercise = 19690720
+;; solution: (52 96)
+(defun what-noun-verb-causes (raw-input target)
+  (let ((possibilities
+          (loop for noun from 0 upto 99
+                append (loop for verb from 0 upto 99
+                             collect (list (list 1 noun) (list 2 verb))))))
+    (loop for possibility in possibilities
+          for memory = (modify-memory (copy-seq raw-input) possibility)
+          for computer = (compute memory)
+          ;; do (format t "Testing ~A ~&" possibility)
+          when (= target (peek computer 0))
+            do (return (mapcar #'second possibility)))))
