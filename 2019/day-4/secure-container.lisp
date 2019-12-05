e51b1786250a11db3042c21a6e4bc84f10a55236
Mark Simpson <verdammelt@gmail.com>
2019-12-04 20:56:03 -0500

    Day 4.2 - DONE

--- 2019/day-4/secure-container.lisp
+++ 2019/day-4/secure-container.lisp
@@ -7,9 +7,19 @@
 
 (defun pairs-of (seq) (map 'list #'cons seq (subseq seq 1)))
 
+(defun tuples-of (seq n)
+  (loop for i from 0 upto (- (length seq) n)
+        collect (subseq seq i (+ i n))))
+
+(defun two-but-not-three-equal-p (guess)
+  (let* ((pairs (tuples-of (coerce guess 'list) 2))
+         (equal-pairs (remove-if-not #'(lambda (pair) (apply #'char= pair)) pairs)))
+    (remove-if #'(lambda (pair) (> (count pair equal-pairs :test #'equal) 1)) equal-pairs)))
+
 (defun has-equal-pair-p (guess)
-  (let ((pairs (pairs-of guess)))
-    (some #'(lambda (pair) (char= (car pair) (cdr pair))) pairs)))
+  (let ((pairs (tuples-of (coerce guess 'list) 2)))
+    (loop for pair in pairs
+          when (apply #'char= pair) collect pair)))
 
 (defun never-decreasing-p (guess)
   (let ((pairs (pairs-of guess)))
@@ -17,7 +27,8 @@
 
 (defun check-guess (guess)
   (and (= (length guess) 6)
-       (has-equal-pair-p guess)
+       ;; (has-equal-pair-p guess)
+       (two-but-not-three-equal-p guess)
        (never-decreasing-p guess)))
 
 (defun format-guess (guess)
