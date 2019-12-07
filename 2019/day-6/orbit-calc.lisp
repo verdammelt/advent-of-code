bb6fe02e4da4373102b40c6395da9edcb34e62d5
Mark Simpson <verdammelt@gmail.com>
2019-12-06 20:55:59 -0500

    Day 6.2 - DONE.

--- 2019/day-6/orbit-calc.lisp
+++ 2019/day-6/orbit-calc.lisp
@@ -29,7 +29,7 @@
     (setf (gethash "COM" orbits) nil)
     orbits))
 
-(defun parent-orbit (orbits sat)
+(defun orbiting (orbits sat)
   (gethash sat orbits))
 
 (defun load-orbits (datafile)
@@ -42,13 +42,30 @@
               orbit-pairs
               :initial-value (make-initial-orbits)))))
 
+(defun orbit-path (orbits sat)
+  "Orbital 'path' from satellite SAT to center-of-masss COM"
+  (labels ((path-to-com (obj path)
+             (if (not (orbiting orbits obj)) (push obj path)
+                 (path-to-com (orbiting orbits obj) (push obj path)))))
+    (path-to-com sat (list))))
+
 (defun orbit-count (orbits)
   (labels ((counter (obj count)
-             (if (not (parent-orbit orbits obj)) count
-                 (counter (parent-orbit orbits obj) (1+ count)))))
+             (if (not (orbiting orbits obj)) count
+                 (counter (orbiting orbits obj) (1+ count)))))
     (loop for key being the hash-key of orbits
        sum (counter key 0))))
 
-(defun checksum (orbits)
-  (declare (ignore orbits))
-  "CHECKSUM is calcuated by summing up the direct and indirect orbits in ORBITS")
+
+(defun orbit-checksum (orbits)
+  "CHECKSUM is calcuated by summing up the direct and indirect orbits in ORBITS"
+  (orbit-count orbits))
+
+(defun orbit-transfers (orbits from to)
+  (let ((from-path (orbit-path orbits from))
+        (to-path (orbit-path orbits to)))
+    (append (set-difference from-path to-path :test #'equal)
+            (nreverse (set-difference to-path from-path :test #'equal)))))
+
+(defun num-orbit-transfers (orbits from to)
+  (- (length (orbit-transfers orbits from to)) 2))
