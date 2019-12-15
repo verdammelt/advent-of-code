f606dee13413690a639c90f063ef67e8f8af13e6
Mark Simpson <verdammelt@gmail.com>
2019-12-15 10:07:55 -0500

    Improvements to string-utils

--- 2019/string-utils.lisp
+++ 2019/string-utils.lisp
@@ -1,14 +1,17 @@
 (defpackage :string-utils
   (:use :common-lisp)
-  (:export :split))
+  (:export :split :split-if))
 
 (in-package :string-utils)
 
-(defun split (string &optional (delimeter #\Space))
+(defun split-if (string predicate)
   (labels ((split-string-iter (string result)
-             (let ((idx (position delimeter string :from-end t)))
+             (let ((idx (position-if predicate string :from-end t)))
                (if idx
                    (split-string-iter (subseq string 0 idx)
                                       (cons (subseq string (1+ idx)) result))
                    (cons string result)))))
     (split-string-iter string (list))))
+
+(defun split (string &optional (delimeter #\Space))
+  (split-if string #'(lambda (c) (char= c delimeter))))
