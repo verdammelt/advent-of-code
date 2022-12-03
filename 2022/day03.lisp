325efc7a123e378829847c85523ea9370ed524af
Mark Simpson <verdammelt@gmail.com>
2022-12-03 11:12:46 -0500

    2022.03 cleanup

--- 2022/day03.lisp
+++ 2022/day03.lisp
@@ -5,6 +5,12 @@
 
 (aoc:def-today-suite*)
 
+(defun inter-section (&rest lists)
+  "Like CL:INTERSECTION but can operate on any number of lists. Evaluates to those
+elements which are common to all the lists. (Note: just like INTERSECTION this
+may return a list with duplicate items)"
+  (reduce #'intersection (cdr lists) :initial-value (car lists)))
+
 (defun read-data (file) (aoc:read-data file
                                        :pre-process #'(lambda (ls) (remove-if #'aoc:empty-string-p ls))
                                        :line-parser #'(lambda (l) (coerce l 'list))))
@@ -21,17 +27,15 @@
          (upper-base 27))
     (+ idx (if (upper-case-p item) upper-base lower-base))))
 
-(defun make-rucksack (cs)
-  (let* ((half (/ (length cs) 2)))
-    (list (subseq cs 0 half) (subseq cs half))))
-
 (defun in-both-compartments (rucksack)
-  (first (intersection (first rucksack) (second rucksack))))
+  (let ((half (/ (length rucksack) 2)))
+    (remove-duplicates
+     (inter-section (subseq rucksack 0 half) (subseq rucksack half)))))
 
 (defun part1 (rucksacks)
   (aoc:sum (mapcar #'priority
-                   (mapcar #'in-both-compartments
-                           (mapcar #'make-rucksack rucksacks)))))
+                   (apply #'concatenate 'list
+                          (mapcar #'in-both-compartments rucksacks)))))
 
 (5am:def-test part1 (:suite :aoc-2022-03)
   (5am:is (= 157 (part1 +example+)))
@@ -41,11 +45,12 @@
   (loop for (x y z) on list by #'cdddr collect (list x y z)))
 
 (defun group-token (group)
-  (first (intersection (first group)
-                       (intersection (second group) (third group)))))
+  (remove-duplicates (apply #'inter-section group)))
 
 (defun part2 (input)
-  (aoc:sum (mapcar #'priority (mapcar #'group-token (by-3s input)))))
+  (aoc:sum (mapcar #'priority
+                   (apply #'concatenate 'list
+                          (mapcar #'group-token (by-3s input))))))
 
 (5am:def-test part2 (:suite :aoc-2022-03)
   (5am:is (= 70 (part2 +example+)))
