4211f275ae1bfc6453f13f323ef21b0ad7ce5693
Mark Simpson <verdammelt@gmail.com>
2022-12-07 18:22:26 -0500

    2022.07 -- cleanup
    Making my implementation more consistent with even more CLOS.

--- 2022/day07.lisp
+++ 2022/day07.lisp
@@ -8,23 +8,21 @@
 (defclass log-line ()
   ((type :initarg :type :reader log-type)
    (data :initarg :data :reader log-data)))
+(defmethod print-object ((obj log-line) stream)
+  (print-unreadable-object (obj stream)
+    (format stream "~A: ~A" (log-type obj) (log-data obj))))
 
 (defclass cd-log (log-line)
   ((type :initform :cd)))
 (defclass ls-log (log-line)
   ((type :initform :ls)))
+(defmethod print-object ((obj ls-log) stream)
+  (print-unreadable-object (obj stream)
+    (format stream "~A" (log-type obj))))
 (defclass dir-log (log-line)
   ((type :initform :dir)))
 (defclass file-log (log-line)
   ((type :initform :file)))
-
-(defmethod print-object ((obj log-line) stream)
-  (print-unreadable-object (obj stream)
-    (format stream "~A: ~A" (log-type obj) (log-data obj))))
-(defmethod print-object ((obj ls-log) stream)
-  (print-unreadable-object (obj stream)
-    (format stream "~A" (log-type obj))))
-
 (defmethod initialize-instance :after ((obj file-log) &rest args &key &allow-other-keys)
   (declare (ignore args))
   (let ((data (slot-value obj 'data)))
@@ -51,47 +49,58 @@
 (defparameter +input+
   (read-data (aoc:today-data-pathname)))
 
-(defun make-node (key value) (cons key value))
-(defun make-dir (dir) (make-node dir nil))
-(defun add-dir (node dir) (push (make-dir dir) node))
-(defun add-file (node file)
-  (if (assoc :files node)
-      (push file (cdr (assoc :files node)))
-      (push (cons :files (list file)) node))
-  node)
-(defun make-tree (root) (list (make-dir root)))
-(defun get-node (tree path)
-  (cond ((null tree) nil)
-        ((null path) nil)
-        ((null (rest path)) (cdr (assoc (first path) tree :test #'equal)))
-        (t (get-node (cdr (assoc (first path) tree :test #'equal)) (rest path)))))
+;;;
+;;; FileSystem simulation
+;;;
+(defclass fs-node () ((name :initarg :name :reader name)))
+(defmethod initialize-instance :after ((obj fs-node) &rest _args &key &allow-other-keys)
+  (declare (ignore _args))
+  (unless (slot-boundp obj 'name) (error "NAME must be provided when creating an FS-NODE")))
+(defgeneric order-nodes (fs-node1 fs-node2))
+(defmethod order-nodes ((fs-node1 fs-node) (fs-node2 fs-node))
+  (string< (name fs-node1) (name fs-node2)))
+(defgeneric size (fs-node))
 
-(defun (setf get-node) (new-value tree path)
-  (cond ((null tree) (error "tree is null!"))
-        ((null path) (error "path is null!"))
-        ((null (rest path)) (setf (cdr (assoc (first path) tree :test #'equal)) new-value))
-        (t (setf (get-node (cdr (assoc (first path) tree :test #'equal)) (rest path))
-                 new-value))))
+(defclass dir-node (fs-node) ((contents :accessor contents :initform nil)))
+(defmethod print-object ((fs-node dir-node) stream)
+  (print-unreadable-object (fs-node stream)
+    (format stream "DIR: ~A: ~A" (name fs-node) (sort (copy-seq (contents fs-node)) #'order-nodes))))
+(defun dir-node-p (fs-node) (typep fs-node 'dir-node))
+(defgeneric subdirs (fs-node))
+(defmethod subdirs ((fs-node dir-node))
+  (remove-if-not #'dir-node-p (contents fs-node)))
+(defun make-dir (name) (make-instance 'dir-node :name name))
+(defun add-to-dir (dir node) (push node (contents dir)))
+(defmethod size ((fs-node dir-node))
+  (reduce #'+ (mapcar #'size (contents fs-node))))
 
-(defun modify-node (tree path fn)
-  "Replaces node at PATH in TREE with the value FN on that node."
-  (let ((current-node (get-node tree path)))
-    (setf (get-node tree path) (funcall fn current-node))))
+(defclass file-node (fs-node) ((size :reader file-size :initarg :size)))
+(defmethod print-object ((fs-node file-node) stream)
+  (print-unreadable-object (fs-node stream)
+    (format stream "FILE: ~A: ~A" (name fs-node) (size fs-node))))
+(defmethod initialize-instance :after ((obj file-node) &rest _args &key &allow-other-keys)
+  (declare (ignore _args))
+  (unless (slot-boundp obj 'size) (error "FILE-SIZE must be provided when creating a FILE-NODE")))
+(defun make-file (name size) (make-instance 'file-node :name name :size size))
+(defmethod size ((fs-node file-node)) (file-size fs-node))
 
 (defclass file-system ()
   ((cwd :initform (list "/") :accessor cwd)
-   (file-tree :initform (make-tree "/")
-              :reader file-tree)))
-
+   (file-tree :initform (make-dir "/") :reader file-tree)))
 (defun make-file-system () (make-instance 'file-system))
-
-(defvar *print-file-tree* nil)
 (defmethod print-object ((obj file-system) stream)
   (print-unreadable-object (obj stream :type t)
-    (format stream "CWD: ~A " (cwd obj))
-    (when *print-file-tree*
-      (format stream "~&TREE: ~A " (file-tree obj)))))
+    (format stream "CWD: ~A " (cwd obj))))
 
+(defgeneric get-current-dir (file-system))
+(defmethod get-current-dir (file-system)
+  (let ((path (rest (reverse (cwd file-system))))) ;; skip "/"
+    (reduce #'(lambda (n d) (find d (contents n) :key #'name :test #'string=)) path
+            :initial-value (file-tree file-system))))
+
+;;;
+;;; Create the File System
+;;;
 (defgeneric process-log-line (file-system log-line))
 (defmethod process-log-line :around (file-system log-line)
   "Wrap processing a log line to ensure the FILE-SYSTEM argument is returned"
@@ -108,13 +117,13 @@
          (push (log-data log-line) (cwd file-system)))))
 (defmethod process-log-line (file-system (log-line dir-log))
   "Process a DIR-LOG - add an empty DIR to our tree"
-  (modify-node (file-tree file-system) (reverse (cwd file-system))
-               #'(lambda (node) (add-dir node (log-data log-line)))))
+  (add-to-dir (get-current-dir file-system)
+              (make-dir (log-data log-line))))
 
 (defmethod process-log-line (file-system (log-line file-log))
   "Process a FILE-LOG - add the file to our tree"
-  (modify-node (file-tree file-system) (reverse (cwd file-system))
-               #'(lambda (node) (add-file node (log-data log-line)))))
+  (add-to-dir (get-current-dir file-system)
+              (apply #'make-file (log-data log-line))))
 
 (defmethod process-log-line (file-system log-line)
   (format *error-output* "Unknown Command: ~A~&" log-line))
@@ -122,43 +131,28 @@
 (defun process-log-lines (lines)
   (reduce #'process-log-line lines :initial-value (make-file-system)))
 
-(defun directory-size (node)
-  "A directory's size is the sum of all the files in the directory recursively."
-  (cond ((null node) 0)
-        ((eq :files (first node)) (reduce #'+ (cdr node) :key #'second))
-        ((stringp (first node)) (directory-size (cdr node)))
-        (t (reduce #'+ (mapcar #'directory-size node)))))
-
-(defun directories (tree) (remove :files tree :key #'first))
-
-(defun sizes-of-directories (nodes sizes)
-  (let* ((dirs (directories nodes))
-         (this-dir (first dirs)))
-    (if (null this-dir) sizes
-        (sizes-of-directories (append (rest dirs) (cdr this-dir))
-                              (append (list (list (first this-dir)
-                                                  (directory-size this-dir)))
-                                      sizes)))))
+(defun sizes-of-file-system (file-system)
+  (labels ((recursive-sizes (dirs sizes)
+             (let ((this-dir (first dirs)))
+               (if (null this-dir) sizes
+                   (recursive-sizes (append (rest dirs) (subdirs this-dir))
+                                        (append (list (list (name this-dir) (size this-dir)))
+                                                sizes))))))
+    (recursive-sizes (list (file-tree file-system)) (list))))
 
 (defun part1 (input)
-  (let ((file-system (process-log-lines input)))
-    (aoc:sum
-     (mapcar #'second
-             (remove-if #'(lambda (n) (> n 100000))
-                        (remove "/"
-                                (sizes-of-directories (file-tree file-system) (list))
-                                :key #'first
-                                :test #'string=)
-                        :key #'second)))))
+  (aoc:sum
+   (mapcar #'second
+           (remove-if #'(lambda (n) (> n 100000))
+                      (sizes-of-file-system (process-log-lines input)) :key #'second))))
 
 (5am:def-test part1 (:suite :aoc-2022-07)
   (5am:is (= 95437 (part1 +example+)))
   (5am:is (= 1306611 (part1 +input+))))
 
 (defun part2 (input)
-  (let* ((file-system (process-log-lines input))
-         (needed-free-space 30000000)
-         (sizes (sort (sizes-of-directories (file-tree file-system) (list)) #'< :key #'second))
+  (let* ((needed-free-space 30000000)
+         (sizes (sort (sizes-of-file-system (process-log-lines input)) #'< :key #'second))
          (current-available (- 70000000 (second (assoc "/" sizes :test #'string=))))
          (smallest-but-big-enough
            (find-if #'(lambda (size) (<= needed-free-space (+ (second size) current-available)))
