b6b89633ecc5e7eca80badf29becb766a7cb8cea
Mark Simpson <verdammelt@gmail.com>
2022-12-14 15:03:53 -0500

    Replace AOC:DIJKSTRA with 2022.12 DIJKSTRA (which actually works!)
    It does not however find all paths - but that's OK for now.

--- 2022/day12.lisp
+++ 2022/day12.lisp
@@ -59,49 +59,10 @@ neighbors of NODE"
   (loop for row below (array-dimension graph 0)
         append (loop for col below (array-dimension graph 1)
                      collect (list row col))))
-
-(defun dijkstra (graph start end neighbors cost all-vertexes)
-  "Find the shortest path between START and END on GRAPH.
-
-NEIGHBORS is a function that takes the GRAPH and a node and returns a list of
-nodes accessible on GRAPH from NODE.
-
-COST is a function that takes GRAPH and two nodes and return the cost of
-traveling between those nodes or NIL if there is no path.
-
-ALL-VERTEXES is a function that takes GRAPH and returns a list of all nodes of
-the GRAPH."
-  (let ((queue (funcall all-vertexes graph))
-        (dist (make-hash-table :test #'equalp))
-        (prev (make-hash-table :test #'equalp)))
-    (setf (gethash start dist) 0)
-    (labels ((less-than (x y) (cond ((and x y) (< x y))
-                                    ((null x) nil)
-                                    (t t)))
-             (pop-least ()
-               (setf queue (sort queue #'less-than :key #'(lambda (node) (gethash node dist))))
-               (pop queue))
-             (path-to (node)
-               (append (list node)
-                       (loop for next = (gethash node prev) then (gethash next prev)
-                             until (not next)
-                             collect next)))
-             (plus-costs (x y) (if (and x y) (+ x y) nil)))
-      (loop while (plusp (length queue))
-            for u = (pop-least)
-            when (equalp u end)
-              do (return (values (gethash u dist) (path-to u)))
-            do (loop for v in (funcall neighbors graph u)
-                     do (let ((alt (plus-costs (gethash u dist)
-                                               (funcall cost graph u v))))
-                          (when (less-than alt (gethash v dist))
-                            (setf (gethash v dist) alt)
-                            (setf (gethash v prev) u))))))))
-
 (defun part1 (input)
   (multiple-value-bind (start end map) (extract-start-end input)
     (multiple-value-bind (length _path)
-        (dijkstra map start end #'neighbors #'cost #'all-vertexes)
+        (aoc:dijkstra map start end #'neighbors #'cost #'all-vertexes)
       (declare (ignore _path))
       length)))
 
@@ -120,9 +81,9 @@ the GRAPH."
     (do ((starts (all-vertexes-with-a map) (rest starts))
          (min most-positive-fixnum))
         ((null starts) min)
-      (let ((length (dijkstra map (first starts) end #'neighbors #'cost #'all-vertexes)))
+      (let ((length (aoc:dijkstra map (first starts) end #'neighbors #'cost #'all-vertexes)))
         (when length (setf min (min length min)))))))
 
 (5am:def-test part2 (:suite :aoc-2022-12)
   (5am:is (= 29 (part2 +example+)))
-  (5am:skip "(5am:is (= 345 (part2 +input+))) because it takes ~~3 min"))
+  (5am:skip "(5am:is (= 345 (part2 +input+))) ;; [2022.12] because it takes ~~3 min"))
--- aoc/algorithms/dijkstra.lisp
+++ aoc/algorithms/dijkstra.lisp
@@ -1,67 +1,40 @@
 (in-package :aoc)
 
-#|
-function Dijkstra(Graph, source):
-  for each vertex v in Graph.Vertices:
-    dist[v] ← INFINITY
-    prev[v] ← UNDEFINED
-    add v to Q
-  dist[source] ← 0
+;; TODO: expand to also be useful to get all paths with their distances from start.
+(defun dijkstra (graph start end neighbors cost all-vertexes)
+  "Find the shortest path between START and END on GRAPH.
 
-  while Q is not empty:
-    u ← vertex in Q with min dist[u]
-    remove u from Q
+NEIGHBORS is a function that takes the GRAPH and a node and returns a list of
+nodes accessible on GRAPH from NODE.
 
-    for each neighbor v of u still in Q:
-      alt ← dist[u] + Graph.Edges(u, v)
-      if alt < dist[v]:
-        dist[v] ← alt
-        prev[v] ← u
+COST is a function that takes GRAPH and two nodes and return the cost of
+traveling between those nodes or NIL if there is no path.
 
-  return dist[], prev[]
-|#
-
-(defun vertices-from-edges (edges)
-  (remove-duplicates (reduce #'append (mapcar #'first edges))))
-
-(defun less-than (x y)
-  "Less than handling possible NIL values. NIL == Infinity"
-  (cond ((null y) t)
-        ((null x) nil)
-        (t (< x y))))
-
-(defun make-costs () (list))
-(defun cost-of (costs x)
-  (cdr (assoc x costs)))
-
-(defun make-queue (items) items)
-(defun queue-empty-p (queue) (zerop (length queue)))
-(defun pop-least (queue costs)
-  (setf queue (sort queue #'less-than :key #'(lambda (x) (cost-of costs x))))
-  (list (pop queue) queue))
-(defun in-queue-p (queue item) (member item queue))
-
-(defun neighbors-of (edges v)
-  (apply #'append (mapcar #'cdar (remove-if-not #'(lambda (edge) (eq (caar edge) v)) edges))))
-(defun edge-cost (edges u v)
-  (let ((cost (cdr (assoc (list u v) edges :test #'equalp))))
-    (if cost cost (error "No edge between ~A and ~A found" u v))))
-
-(defun dijkstra (edges source)
-  (let ((vertices (make-queue (vertices-from-edges edges)))
-        (dist (make-costs))
-        (prev (list)))
-    (push (cons source 0) dist)
-
-    (loop while (not (queue-empty-p vertices))
-          for (u new-queue) = (pop-least vertices dist)
-          do (setf vertices new-queue)
-
-          do (loop for v in (remove-if-not #'(lambda (x) (in-queue-p vertices x))
-                                           (neighbors-of edges u))
-                   for alt = (+ (cost-of dist u) (edge-cost edges u v))
-                   when (less-than alt (cost-of dist v))
-                     do (progn (push (cons v alt) dist)
-                               (push (cons v u) prev)))
-
-          finally (return (values dist prev)))))
+ALL-VERTEXES is a function that takes GRAPH and returns a list of all nodes of
+the GRAPH."
+  (let ((queue (funcall all-vertexes graph))
+        (dist (make-hash-table :test #'equalp))
+        (prev (make-hash-table :test #'equalp)))
+    (setf (gethash start dist) 0)
+    (labels ((less-than (x y) (cond ((and x y) (< x y))
+                                    ((null x) nil)
+                                    (t t)))
+             (pop-least ()
+               (setf queue (sort queue #'less-than :key #'(lambda (node) (gethash node dist))))
+               (pop queue))
+             (path-to (node)
+               (append (list node)
+                       (loop for next = (gethash node prev) then (gethash next prev)
+                             until (not next)
+                             collect next)))
+             (plus-costs (x y) (if (and x y) (+ x y) nil)))
+      (loop while (plusp (length queue))
+            for u = (pop-least)
+            when (equalp u end)
+              do (return (values (gethash u dist) (path-to u)))
+            do (loop for v in (funcall neighbors graph u)
+                     do (let ((alt (plus-costs (gethash u dist)
+                                               (funcall cost graph u v))))
+                          (when (less-than alt (gethash v dist))
+                            (setf (gethash v dist) alt)
+                            (setf (gethash v prev) u))))))))
