e3be171fbd438f6afe6a8ae6fd87e2231a5cb3ce
Mark Simpson <verdammelt@gmail.com>
2023-12-04 17:31:24 -0500

    2023.04
    Part 2 started with a naive recursive solution of having a to-process
    list and working down it, sometimes appending more work to it. This
    worked OK with the example but was not going to be fast enough (less
    than a couple minutes) on the input.
    
    Searched for hints which I didn't understand, finally after reading
    through a few solutions I started to get the idea of it. For each card
    N with wins W add its count to cards N+1..N+W. Then process card N+1
    until done. Now sum the counts.

---
+++ 2023/day04.lisp
@@ -0,0 +1,64 @@
+(defpackage #:aoc-2023-04
+  (:use :cl))
+
+(in-package #:aoc-2023-04)
+
+(aoc:def-today-suite*)
+
+(defun card-number (card) (first card))
+(defun card-winning-numbers (card) (second card))
+(defun card-your-numbers (card) (third card))
+
+(defun string-of-numbers->list-of-numbers (str &optional (delimiter #\Space))
+  (mapcar #'parse-integer (aoc:split-string-on-char delimiter str)))
+
+(defun parse-scratch-card (str)
+  (let ((raw-card (aoc:split-string-on-chars '(#\: #\|) str)))
+    (list (parse-integer (subseq (first raw-card) (length "card")))
+          (string-of-numbers->list-of-numbers (second raw-card))
+          (string-of-numbers->list-of-numbers (third raw-card)))))
+
+(defun read-data (file) (aoc:read-data file :line-parser #'parse-scratch-card))
+
+(defparameter +example+
+  (read-data (aoc:today-data-pathname "example")))
+
+(defparameter +input+
+  (read-data (aoc:today-data-pathname)))
+
+(defun number-of-wins (card)
+  (length (intersection (card-winning-numbers card) (card-your-numbers card))))
+
+(defun points-for-card (card)
+  (if (< 0 (number-of-wins card))
+      (expt 2 (1- (number-of-wins card)))
+      0))
+
+(defun part1 (input)
+  (aoc:sum (mapcar #'points-for-card input)))
+
+(5am:def-test part1 (:suite :aoc-2023-04)
+  (5am:is (= 13 (part1 +example+)))
+  (5am:is (= 20107 (part1 +input+))))
+
+(defun card-and-wins (cards)
+  (loop for card in cards
+        for x = 1 then (1+ x)
+        collect (list x (number-of-wins card))))
+
+(defun total-up-cards (card-wins)
+  "CARD-WINS is a list of (CARD-NUM NUM-WINS).
+Create a list of counts for cards - initially all 1. For each card N with wins W
+increment count of each card N+1..N+w by the count of card N."
+  (let ((num-cards (make-list (length card-wins) :initial-element 1)))
+    (loop for (num wins) in card-wins
+          do (dotimes (i wins)
+               (incf (nth (+ num i) num-cards) (nth (1- num) num-cards))))
+    (aoc:sum num-cards)))
+
+(defun part2 (input)
+  (total-up-cards (card-and-wins input)))
+
+(5am:def-test part2 (:suite :aoc-2023-04)
+  (5am:is (= 30 (part2 +example+)))
+  (5am:is (= 8172507 (part2 +input+))))
