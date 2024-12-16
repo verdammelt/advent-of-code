9cabdfbc174b6eeeb97e551e8331dd10fd7c0255
Mark Simpson <verdammelt@gmail.com>
2024-12-15 20:31:57 -0500

    2024-12.2 complete
    Could not figure it out myself so i adapted the solution from:
    https://git.thomasvoss.com/advent-of-code/tree/2024/12/puzzles.lisp
    for my code.

--- 2024/day12.lisp
+++ 2024/day12.lisp
@@ -22,77 +22,123 @@
 
 (defun plant-equal (p1 p2) (and p1 p2 (char= p1 p2)))
 
-(defun make-region (map plant)
-  (list :map map :plant plant :plots (list)))
 
-(defun region-map (region) (getf region :map))
-(defun region-plant (region) (getf region :plant))
-(defun region-plots (region) (getf region :plots))
-(defun (setf region-plots) (new-value region) (setf (getf region :plots) new-value))
-(defun region-perimeter (region)
-  (let ((plots (region-plots region))
-        (plant (region-plant region))
-        (map (region-map region)))
-    (count-if-not #'(lambda (p) (plant-equal plant p))
-                  (mapcan
-                   #'(lambda (p) (mapcar #'(lambda (n) (aoc:coord-safe-aref map n)) (coord-neighbors p)))
-                   plots))))
-(defun region-area (region) (length (region-plots region)))
+(defun gather-regions (map perimeter-count-fn)
+  (let ((regions (list))
+        (seen (make-hash-table :test #'equalp))) ;; PLOT -> T
+    (labels ((find-same-plant-neighbors (target-plant plot)
+               (loop for n in (list (aoc:coord-add plot (aoc:make-coord -1 0))
+                                    (aoc:coord-add plot (aoc:make-coord 0 1))
+                                    (aoc:coord-add plot (aoc:make-coord 1 0))
+                                    (aoc:coord-add plot (aoc:make-coord 0 -1)))
+                     when (plant-equal target-plant
+                                       (aoc:coord-safe-aref map n))
+                       collect n))
+             (process-plot (target-plant plot)
+               "from https://git.thomasvoss.com/advent-of-code/tree/2024/12/puzzles.lisp"
+               (when (gethash plot seen) (return-from process-plot (list 0 0)))
 
-(defun plot-in-region (plot region)
-  (member plot (region-plots region) :test #'aoc:coord-equal))
+               (setf (gethash plot seen) t)
 
-(defun coord-neighbors (coord)
-  (mapcar #'(lambda (dir) (aoc:coord-add coord dir))
-          (list (aoc:make-coord -1 0)
-                (aoc:make-coord 0 1)
-                (aoc:make-coord 1 0)
-                (aoc:make-coord 0 -1))))
+               (when (not (plant-equal target-plant
+                                       (aoc:coord-safe-aref map plot)))
+                 (return-from process-plot (list 0 0)))
 
-(defun gather-regions (map)
-  (do ((all-plots (all-coords-for-map map))
-       (all-regions (list))
-       (working-edge (list))
-       (current-region nil)
-       (next nil))
-      ((null all-plots) (push current-region all-regions))
+               (let ((neighbors
+                       (find-same-plant-neighbors target-plant plot)))
+                 (loop with area = 1
+                       with perimeter = (funcall perimeter-count-fn map plot neighbors)
+                       for neighbor in neighbors
+                       do (destructuring-bind (a1 p1)
+                              (process-plot target-plant neighbor)
+                            (incf area a1)
+                            (incf perimeter p1))
+                       finally (return (list area perimeter))))))
 
-   (cond
-      ((not current-region)
-       (setf working-edge (list (car all-plots))
-             current-region (make-region map (aoc:coord-safe-aref map (car working-edge)))))
+      (aoc:map-2d-array
+       #'(lambda (m x y)
+           (let* ((plot (aoc:make-coord x y))
+                  (plant (aoc:coord-safe-aref m plot))
+                  (area-and-perimeter (process-plot plant plot)))
+             (unless (equal '(0 0) area-and-perimeter)
+               (push (list plant area-and-perimeter) regions))))
+       map))
+    (nreverse regions)))
 
-      (next
-       (when (plant-equal (aoc:coord-safe-aref map next) (region-plant current-region))
-         (pushnew next (region-plots current-region) :test #'aoc:coord-equal)
-         (setf all-plots (remove next all-plots :test #'aoc:coord-equal))
-         (let ((neighbors (coord-neighbors next)))
-           (setf working-edge (append working-edge
-                                      (remove-if
-                                       #'(lambda (p) (plot-in-region p current-region))
-                                       neighbors)))))
-       (setf next nil))
-
-      (working-edge (setf next (car working-edge)
-                          working-edge (cdr working-edge)))
-
-      (t (push current-region all-regions)
-         (setf current-region nil)))))
+(defun simple-perimeter-count (map plot neighbors)
+  (declare (ignore map))
+  (declare (ignore plot))
+  (- 4 (length neighbors)))
 
 (defun part1 (input)
-  (let ((regions (gather-regions input)))
-    (aoc:sum (mapcar #'(lambda (r) (* (region-area r) (region-perimeter r))) regions))))
+  (let ((regions (gather-regions input #'simple-perimeter-count)))
+    ;; REGIONS is (PLANT (AREA PERIMETER))
+    (aoc:sum (mapcar #'aoc:product (mapcar #'second regions)))))
 
 (5am:def-test part1 (:suite :aoc-2024-12)
   (5am:is (= 1930 (part1 +example+)))
-  (5am:skip ":aoc-2024-12.1 does not work on real input")
-  ;; (5am:is (= -1 (part1 +input+)))
-  )
+  (5am:is (= 1533024 (part1 +input+))))
 
-(defun part2 (input) (declare (ignore input)) 0)
+(defun corner-count (map plot neighbors)
+  "from https://git.thomasvoss.com/advent-of-code/tree/2024/12/puzzles.lisp"
+  (case (length neighbors)
+    (0 4)
+    (1 2)
+    (2 (let ((left (first neighbors))
+             (right (second neighbors)))
+         (cond ((or (= (aoc:coord-x left)
+                       (aoc:coord-x right)
+                       (aoc:coord-x plot))
+                    (= (aoc:coord-y left)
+                       (aoc:coord-y right)
+                       (aoc:coord-y plot)))
+                0)
+               ((plant-equal (aoc:coord-safe-aref map plot)
+                             (aoc:coord-safe-aref
+                              map
+                              (aoc:make-coord
+                               (if (= (aoc:coord-x left) (aoc:coord-x plot))
+                                   (aoc:coord-x right) (aoc:coord-x left))
+                               (if (= (aoc:coord-y left) (aoc:coord-y plot))
+                                   (aoc:coord-y right) (aoc:coord-y left)))))
+                1)
+               (t 2))))
+    (3 (let ((wart (loop for n in neighbors
+                         if (/= (aoc:coord-x n) (aoc:coord-x plot))
+                           collect n into odd-one-out-x
+                         else
+                           collect n into odd-one-out-y
+                         finally (return (first (if (= 1 (length odd-one-out-x))
+                                                    odd-one-out-x
+                                                    odd-one-out-y))))))
+         (loop for n in neighbors
+               count (not (or (equal n wart)
+                              (plant-equal
+                               (aoc:coord-safe-aref map plot)
+                               (aoc:coord-safe-aref
+                                map
+                                (aoc:make-coord
+                                 (if (= (aoc:coord-x n)
+                                        (aoc:coord-x plot))
+                                     (aoc:coord-x wart)
+                                     (aoc:coord-x n))
+                                 (if (= (aoc:coord-y n)
+                                        (aoc:coord-y plot))
+                                     (aoc:coord-y wart)
+                                     (aoc:coord-y n))))))))) )
+    (4 (loop for n in (list (aoc:coord-add plot (aoc:make-coord -1 -1))
+                            (aoc:coord-add plot (aoc:make-coord -1 1))
+                            (aoc:coord-add plot (aoc:make-coord 1 -1))
+                            (aoc:coord-add plot (aoc:make-coord 1 1)))
+             count (not (plant-equal (aoc:coord-safe-aref map n)
+                                     (aoc:coord-safe-aref map plot)))))))
+
+
+(defun part2 (input)
+  (let ((regions (gather-regions input #'corner-count)))
+    ;; REGIONS is (PLANT (AREA PERIMETER))
+    (aoc:sum (mapcar #'aoc:product (mapcar #'second regions)))))
 
 (5am:def-test part2 (:suite :aoc-2024-12)
-  (5am:skip ":aoc-2024-12.2 not implemented")
-  ;; (5am:is (= -1 (part2 +example+)))
-  ;; (5am:is (= -1 (part2 +input+)))
-  )
+  (5am:is (= 1206 (part2 +example+)))
+  (5am:is (= 910066 (part2 +input+))))
